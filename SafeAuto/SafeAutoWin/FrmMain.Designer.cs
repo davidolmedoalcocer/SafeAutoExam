146434cb4ddaf20bddc31de0e74d8681a37a18fe
davidolmedoalcocer <67847904+davidolmedoalcocer@users.noreply.github.com>
2020-11-10 11:51:52 -0600

    Add files via upload

---
+++ SafeAuto/SafeAutoWin/FrmMain.Designer.cs
@@ -0,0 +1,79 @@
+﻿namespace SafeAutoWin
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnOpenFile = new System.Windows.Forms.Button();
+            this.opdOpenFile = new System.Windows.Forms.OpenFileDialog();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnOpenFile
+            // 
+            this.btnOpenFile.Location = new System.Drawing.Point(116, 12);
+            this.btnOpenFile.Name = "btnOpenFile";
+            this.btnOpenFile.Size = new System.Drawing.Size(204, 29);
+            this.btnOpenFile.TabIndex = 0;
+            this.btnOpenFile.Text = "Open File";
+            this.btnOpenFile.UseVisualStyleBackColor = true;
+            this.btnOpenFile.Click += new System.EventHandler(this.btnOpenFile_Click);
+            // 
+            // opdOpenFile
+            // 
+            this.opdOpenFile.FileName = "*.txt";
+            // 
+            // txtResult
+            // 
+            this.txtResult.Location = new System.Drawing.Point(16, 47);
+            this.txtResult.Multiline = true;
+            this.txtResult.Name = "txtResult";
+            this.txtResult.Size = new System.Drawing.Size(408, 170);
+            this.txtResult.TabIndex = 1;
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(436, 239);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.btnOpenFile);
+            this.Name = "FrmMain";
+            this.Text = "SafeAuto";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnOpenFile;
+        private System.Windows.Forms.OpenFileDialog opdOpenFile;
+        private System.Windows.Forms.TextBox txtResult;
+    }
+}
+
---
+++ SafeAuto/SafeAutoWin/FrmMain.cs
@@ -0,0 +1,84 @@
+﻿using SafeAuto.Core.Entities;
+using SafeAuto.Core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+
+namespace SafeAutoWin
+{
+    public partial class FrmMain : Form
+    {
+        private readonly IDriverService _driverService;
+        public FrmMain(IDriverService driverService)
+        {
+            InitializeComponent();
+
+            this._driverService = driverService;
+        }
+
+
+
+        private void btnOpenFile_Click(object sender, EventArgs e)
+        {
+            DialogResult dr = this.opdOpenFile.ShowDialog();
+
+            if (dr == DialogResult.OK)
+            {
+                string fileName = this.opdOpenFile.FileName;
+                string[] lines = File.ReadAllLines(fileName);
+
+                this.ProcessLines(lines);
+
+            }
+        }
+
+        private void ProcessLines(string[] lines)
+        {
+            try
+            {
+                //First clear the textbox
+                this.txtResult.Text = string.Empty;
+
+                StringBuilder resultPrint = new StringBuilder();
+                List<Driver> driversOrder;
+                string displayLine = string.Empty;
+
+                foreach (string line in lines)
+                {
+                    this._driverService.ProcessComand(line);
+                }
+
+
+                driversOrder = this._driverService.GetDriversSorted();
+
+                foreach (Driver driver in driversOrder)
+                {
+                    // Discard any trips that average a speed of less than 5 mph or greater than 100 mph
+                    if (driver.SpeedAverage < 5 || driver.SpeedAverage > 100)
+                    {
+                        displayLine = string.Format("{0}: {1} miles", driver.Name,
+                            Math.Round(driver.Distance, 0));
+                    }
+                    else
+                    {
+                        displayLine = string.Format("{0}: {1} miles @ {2} mph",
+                            driver.Name, Math.Round(driver.Distance, 0), Math.Round(driver.SpeedAverage, 0));
+                    }
+
+                    resultPrint.Append(displayLine + Environment.NewLine);
+                }
+
+
+
+                this.txtResult.Text = resultPrint.ToString();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Exists a problem with the file. " + ex.Message);
+            }
+
+        }
+    }
+}
